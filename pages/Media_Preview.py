8222ada88402f674fb35bd8b91b7c01fa21e9be9
krishna <155258579+Venkykrishna99@users.noreply.github.com>
2025-04-07 22:54:55 +0530

    Update Media_Preview.py

--- pages/Media_Preview.py
+++ pages/Media_Preview.py
@@ -6,8 +6,10 @@ col1, col2 = st.columns(2)
 
 with col1:
     st.subheader("🎧 Audio")
-    st.audio("Audio.mp3")
+    st.audio("lokiverse.mp3")
 
 with col2:
     st.subheader("🎥 Video")
     st.video("video.mp4")
+
+st.image("image.jpg")
